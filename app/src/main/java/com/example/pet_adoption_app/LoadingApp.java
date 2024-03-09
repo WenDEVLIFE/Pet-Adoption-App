36c17e236f9624d51f7420bd993a032c0e9db710
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-09 20:09:27 +0800

    Done Designing the LoadingApp.java

--- app/src/main/java/com/example/pet_adoption_app/LoadingApp.java
+++ app/src/main/java/com/example/pet_adoption_app/LoadingApp.java
@@ -1,6 +1,8 @@
 package com.example.pet_adoption_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoadingApp extends AppCompatActivity {
+    ProgressBar loading;
+    Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,27 @@ public class LoadingApp extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Our loading bar
+        loading = findViewById(R.id.progressBar);
+        loading.setMax(100);
+        loading.setProgress(0);
+
+        // This will load the value of the progress bar
+        thread = new Thread(() -> {
+            try {
+                for (int i = 0; i < 100; i++) {
+                    // Set the progress bar value
+                    loading.setProgress(i);
+                    Thread.sleep(50);
+                }
+                // This will start and go to next page
+                Intent intent = new Intent(LoadingApp.this, MainActivity.class);
+                startActivity(intent);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start(); // Don't forget to start the thread
     }
 }
