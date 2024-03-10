266bbe470c1163b591e748ec757f5aaa1e6e1fed
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-10 10:30:31 +0800

    Added listener for sign up text and sign up button

--- app/src/main/java/com/example/pet_adoption_app/MainActivity.java
+++ app/src/main/java/com/example/pet_adoption_app/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.pet_adoption_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText usernamefield,passwordfield;
+
+    Button Sign_up;
+
+    TextView Sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernamefield = findViewById(R.id.username);
+        passwordfield = findViewById(R.id.password);
+
+        Sign_in = findViewById(R.id.sigin);
+        Sign_in.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Signin_Activity.class);
+            startActivity(intent);
+        });
+
+        Sign_up = findViewById(R.id.button);
+        Sign_up.setOnClickListener(v ->{
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage("Sign up successful");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+
+        });
+
+
+
     }
+
 }
