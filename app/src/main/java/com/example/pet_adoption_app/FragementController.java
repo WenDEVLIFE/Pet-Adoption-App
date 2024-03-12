32c4c9898e01cfebe8400a00bc08b458529ed496
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-12 13:18:21 +0800

    Added a function to send a value from the fragments

--- app/src/main/java/com/example/pet_adoption_app/FragementController.java
+++ app/src/main/java/com/example/pet_adoption_app/FragementController.java
@@ -2,6 +2,7 @@ package com.example.pet_adoption_app;
 
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -18,6 +19,7 @@ import com.google.android.material.navigation.NavigationView;
 
 public class FragementController extends AppCompatActivity {
 
+    TextView usernametext, Name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,14 +34,21 @@ public class FragementController extends AppCompatActivity {
 
         // This will call the home fragment
         replaceFragement(new HomeFragment());
-
         // Get the NavigationView reference using findViewById
         NavigationView navView = findViewById(R.id.navView);
-        ColorStateList csl = ContextCompat.getColorStateList(this, R.color.Cool_gray);
+        usernametext = navView.getHeaderView(0).findViewById(R.id.username);
+        Name = navView.getHeaderView(0).findViewById(R.id.name);
+
+        usernametext.setText("Null");
+        Name.setText("Null");
+        ColorStateList csl = ContextCompat.getColorStateList(this, R.color.white);
         navView.setItemIconTintList(csl);
         navView.setNavigationItemSelectedListener(item -> {
             if(item.getItemId() == R.id.home){
-                replaceFragement(new HomeFragment());
+                // This will go to Home Fragements
+                HomeFragment fragment = new HomeFragment();
+                replaceFragement(fragment);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Home");
                 builder.setMessage("Home Fragment");
@@ -56,7 +65,19 @@ public class FragementController extends AppCompatActivity {
 
             // This will go to Adopt Pet Fragements
             else if(item.getItemId() == R.id.adopt_pet){
-                replaceFragement(new AdoptionFragments());
+                AdoptionFragments fragment = new AdoptionFragments();
+
+                // Step 2: Create a new Bundle
+                Bundle bundle = new Bundle();
+
+                // Step 3: Put the data into the Bundle
+                bundle.putString("username", usernametext.getText().toString());
+
+                // Step 4: Set the arguments for the fragment with the Bundle
+                fragment.setArguments(bundle);
+
+                // Step 5: Use the FragmentManager to start the fragment
+                replaceFragement(fragment);
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Adopt Pet");
                 builder.setMessage("Adopt Pet Fragment");
