e51fbeb464d5c4a8c559d644fddbefea00c2d671
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-13 14:27:31 +0800

    Added AddAdopt fragment_add_adopt.xml 💻

--- app/src/main/java/com/example/pet_adoption_app/AddAdopt.java
+++ app/src/main/java/com/example/pet_adoption_app/AddAdopt.java
@@ -1,29 +1,19 @@
 package com.example.pet_adoption_app;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.appcompat.widget.SearchView;
-
-import android.view.ViewTreeObserver;
-import android.widget.AutoCompleteTextView;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AdoptionFragments#newInstance} factory method to
+ * Use the {@link AddAdopt#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AdoptionFragments extends Fragment {
+public class AddAdopt extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,11 +24,7 @@ public class AdoptionFragments extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private TextView usernametext;
-
-    private String username;
-
-    public AdoptionFragments() {
+    public AddAdopt() {
         // Required empty public constructor
     }
 
@@ -48,11 +34,11 @@ public class AdoptionFragments extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NotificationFragment.
+     * @return A new instance of fragment AddAdopt.
      */
     // TODO: Rename and change types and number of parameters
-    public static AdoptionFragments newInstance(String param1, String param2) {
-        AdoptionFragments fragment = new AdoptionFragments();
+    public static AddAdopt newInstance(String param1, String param2) {
+        AddAdopt fragment = new AddAdopt();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,11 +52,6 @@ public class AdoptionFragments extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
-            // This is a test to get the value from the navbar and send it to the fragments.
-            username = getArguments().getString("username");
-
-
         }
     }
 
@@ -78,22 +59,10 @@ public class AdoptionFragments extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_adopt, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_add_adopt, container, false);
 
-        // Our search view
-        SearchView searchView = rootView.findViewById(R.id.searchView);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return true;
-            }
-        });
 
-        return rootView;
+    return  rootView;
     }
 }
