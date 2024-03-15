ec11109f5591bd62ebdbbe43aded994c4c6da000
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-15 09:44:27 +0800

    Done adding fragment_transaction__history.xml👾

---
+++ app/src/main/java/com/example/pet_adoption_app/Transaction_History.java
@@ -0,0 +1,82 @@
+package com.example.pet_adoption_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Transaction_History#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Transaction_History extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Transaction_History() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Transaction_History.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Transaction_History newInstance(String param1, String param2) {
+        Transaction_History fragment = new Transaction_History();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+         View rootview = inflater.inflate(R.layout.fragment_transaction__history, container, false);
+
+// This is our button back to home
+        ImageButton btnback = rootview.findViewById(R.id.buttonnback);
+        btnback.setOnClickListener(v -> replaceFragement(new HomeFragment()));
+
+         return rootview;
+    }
+
+    private void replaceFragement(Fragment fragment) {
+
+        // Call the fragment manager and begin the transaction to replace the fragment
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
