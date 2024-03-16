c651c2275d893de02ca862726a5d1c29251edcc6
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-16 13:34:23 +0800

    Added Ask_Donations.java and fragment_ask__donations.xml💻

---
+++ app/src/main/java/com/example/pet_adoption_app/Ask_Donations.java
@@ -0,0 +1,67 @@
+package com.example.pet_adoption_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Ask_Donations#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Ask_Donations extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Ask_Donations() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Ask_Donations.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Ask_Donations newInstance(String param1, String param2) {
+        Ask_Donations fragment = new Ask_Donations();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_ask__donations, container, false);
+
+    return rootview;
+    }
+
+}
