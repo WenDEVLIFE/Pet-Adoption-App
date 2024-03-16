a9b62f782a22460b4852e49398c079e2e55213bf
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-16 13:46:43 +0800

    Done designing Fragments_Ask_Donations 💻

---
+++ app/src/main/java/com/example/pet_adoption_app/Fragment_Lost_Pets.java
@@ -0,0 +1,109 @@
+package com.example.pet_adoption_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment_Lost_Pets#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment_Lost_Pets extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment_Lost_Pets() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment_Lost_Pets.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment_Lost_Pets newInstance(String param1, String param2) {
+        Fragment_Lost_Pets fragment = new Fragment_Lost_Pets();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_lost_pets, container, false);
+
+        // Our search view
+        SearchView searchView = rootview.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        // This button serves as a floating action button to add lost pets
+        FloatingActionButton buttonAdd = rootview.findViewById(R.id.floatingActionButton);
+        buttonAdd.setOnClickListener(v->{
+            replaceFragement(new Fragment_Add_Lost_Pets());
+        });
+
+        // this for button back image button
+        ImageButton buttonBack = rootview.findViewById(R.id.buttonnback);
+        buttonBack.setOnClickListener(v->{
+            replaceFragement(new HomeFragment());
+        });
+
+
+        return rootview;
+
+    }
+
+    private void replaceFragement(Fragment fragment) {
+
+        // Call the fragment manager and begin the transaction to replace the fragment
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
